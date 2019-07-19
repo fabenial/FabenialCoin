cb439785298c8e95e27de774edcd7c2fb08e3603
allespro <arcy.all@yahoo.com>
2019-07-19 15:53:21 +0000

    p2p числа
    А так же мусор из папки build

--- build/release/external/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alles/FabenialCoin/Sources/external
-# Build directory: /home/alles/FabenialCoin/Sources/build/release/external
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("miniupnpc")
-subdirs("gtest")
