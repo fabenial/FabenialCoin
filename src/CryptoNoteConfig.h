011d1814234bb3a6730859b3374a4e3bdd220349
allespro <arcy.all@yahoo.com>
2019-07-22 01:00:10 +0000

    emission

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2019 The Cryptonote developers
+// Copyright (c) 2019-2019 The Fabenial developers
 
 #pragma once
 
@@ -23,7 +23,7 @@ const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 //TODO ((uint64_t)(-1)) equals to 18446744073709551616 coins
 //TODO or you can define number explicitly UINT64_C(858986905600000000)
 const uint64_t MONEY_SUPPLY                                  = UINT64_C(2400000000000000); //total coins/8
-const unsigned EMISSION_SPEED_FACTOR                         = 8;
+const unsigned EMISSION_SPEED_FACTOR                         = 21;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
 //TODO Define number of blocks for block size median calculation
